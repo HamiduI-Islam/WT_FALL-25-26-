7fd21551c24d6212481e78ddef95625cc1a62cf9
Hamidul-Islam <hamidulislam150780@gmail.com>
2025-11-06 13:42:57 +0600

    form

--- MID/LAB TASK SUBMISSION/TASK1/Task1.php
+++ MID/LAB TASK SUBMISSION/TASK1/Task1.php
@@ -1,11 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+ 
+    <title>Clinic Patient Registration</title>
 </head>
+
 <body>
-  
+    <center><h2>Clinic Patient Registration</h2></center>
+    <center>
+    <div class="wt">
+        <table>
+            <tr>
+                <td>
+                    Full Name :
+                </td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>
+                    Age:
+                </td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Phone Number:</td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Email Address:</td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Insurance Provider:</td>
+            </tr>
+            <tr>
+                <td>
+                    <select>
+                        <option>Select Provider </option>
+                        <option>Delta Life Insurance</option>
+                        <option>Metlife Insurance Company Limited</option>
+                        <option>Sadharan Bima Corporation</option>
+                    </select>
+                </td>
+            </tr>
+            
+            <tr>
+                <td>Insurance Policy Number:</td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+          
+ 
+            </tr>
+            <tr><td>
+            <center><h3>Additional Information</h3></center></td>
+            </tr>
+            <tr>
+                <td>Username:</td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Confirm Password:</td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+            </tr>
+            <tr><td><center>
+              <button> Register</button><center>
+            </td>
+</tr>
+        </table>
+    </div>
+</center>
+      
 </body>
 </html>
