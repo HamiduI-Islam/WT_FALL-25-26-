147e7e4ecef8802bb3182f333695b0905934c9f3
Hamidul-Islam <hamidulislam150780@gmail.com>
2025-12-31 00:39:55 +0600

    home page created

---
+++ FINAL/Project/Smart Farm Management System/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HOME PAGE</title>
+</head>
+<style>
+  body{
+    background-color: #4cb14cff;;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+  form {
+  width: 350px;
+  margin: 100px auto;  
+  padding: 20px;
+  border: 2px solid green;
+  border-radius: 8px;
+  background-color: white;
+}
+  button {
+    
+    margin: 15px auto;   
+    padding: 10px 30px;
+    border-radius: 5px;
+  }
+
+  </style>
+<body>
+  <form>
+  <h3>WELCOME TO OUR WEBSITE</h3>
+  <br><br><br><br><br><br><br><br>
+  <button>LOGIN</button>
+  <br>
+  <br>
+  <button>Register</button>
+</form>
+</body>
+</html>
