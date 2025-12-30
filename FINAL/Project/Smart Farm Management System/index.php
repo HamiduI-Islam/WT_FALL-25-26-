1a7ddfb99990fc40a548cadd32ec7482e93201c4
Hamidul-Islam <hamidulislam150780@gmail.com>
2025-12-31 01:09:57 +0600

    Button added

--- FINAL/Project/Smart Farm Management System/index.php
+++ FINAL/Project/Smart Farm Management System/index.php
@@ -7,7 +7,7 @@
 </head>
 <style>
   body{
-    background-color: #4cb14cff;;
+    background-color: #4cb14cff;
     font-family: Arial, sans-serif;
     margin: 0;
     padding: 0;
@@ -22,19 +22,42 @@
 }
   button {
     
-    margin: 15px auto;   
-    padding: 10px 30px;
+    margin: 5px auto;   
+    padding: 6px 20px;
     border-radius: 5px;
   }
+  #L1 {
+    background-color: #4bd64bff;
+    width: 150px;          
+    height: 40px;         
+    margin: 0 ;         
+    transition: width 0.3s ease; 
+}
 
+#L1:hover {
+    width: 140px;          
+}
+#L2 {
+    background-color: #4bd64bff;
+    width: 150px;          
+    height: 40px;         
+    margin: 0 ;         
+    transition: width 0.3s ease; 
+}
+
+#L2:hover {
+    width: 140px;          
+}
   </style>
 <body>
   <form>
   <h3>WELCOME TO OUR WEBSITE</h3>
   <br><br><br><br><br><br><br><br>
-  <button>LOGIN</button>
+  <button id="L1">ADMIN LOGIN</button><br><br>
+  <button id="L2">FARMER LOGIN</button>
   <br>
   <br>
+  <p>If you are not a user, please Register !!</p>
   <button>Register</button>
 </form>
 </body>
